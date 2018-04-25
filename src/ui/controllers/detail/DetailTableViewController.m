b22e38032ac519f1bbd8809b3c42130c7705162a
SergioPH <spenin@solidgear.es>
2018-04-25 12:41:15 +0200

    FIRMA-97  Adapt the UI to show the sign line type for iPhone and iPad.

--- src/ui/controllers/detail/DetailTableViewController.m
+++ src/ui/controllers/detail/DetailTableViewController.m
@@ -249,8 +249,7 @@ typedef NS_ENUM (NSInteger, PFDocumentAction)
     self.subject.text = _dataSource.subj;
     self.applicationLbl.text = _dataSource.app;
     [self showRejectExplanationIfExists];
-    NSString *signLinesType = _dataSource.signlinestype;
-    NSLog(@"SSSSSSSSS %@ ",signLinesType);
+    self.signLinesTypeLbl.text = _dataSource.signlinestype;
 
     _selectedRows = nil;
     PFRequest *detailRequest = [[PFRequest alloc] initWithId:_requestId];
