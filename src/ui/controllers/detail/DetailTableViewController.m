33033fb3be64f31482089473144b29242ca36d54
SergioPH <spenin@solidgear.es>
2018-09-13 15:33:55 +0200

    Fixed another deprecated method for alert view with error.

--- src/ui/controllers/detail/DetailTableViewController.m
+++ src/ui/controllers/detail/DetailTableViewController.m
@@ -460,13 +460,11 @@ typedef NS_ENUM (NSInteger, PFDocumentAction)
 - (void)didReceiveError:(NSString *)errorString
 {
     [SVProgressHUD dismiss];
-
     DDLogDebug(@"UnassignedRequestTableViewController::didReceiveParserWithError: %@", errorString);
-    [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"")
-                                message:errorString
-                               delegate:nil
-                      cancelButtonTitle:NSLocalizedString(@"OK", @"")
-                      otherButtonTitles:nil] show];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Alert_View_Error", nil) message:errorString preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:NSLocalizedString(@"Ok", nil) style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:cancel];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 #pragma mark - RequestSignerEvent
