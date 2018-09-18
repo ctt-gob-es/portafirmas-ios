68c472b0ebeafab28762a8c768efceb65cf0e88b
SergioPH <spenin@solidgear.es>
2018-09-18 17:10:41 +0200

    Added space to some code.

--- src/ui/controllers/lists/filter/FilterVC.m
+++ src/ui/controllers/lists/filter/FilterVC.m
@@ -76,7 +76,7 @@ static const CGFloat kFilterVCDefaultMargin = 14.f;
     [_enableFiltersSwitch setFrame:CGRectMake(self.view.frame.size.width - _enableFiltersSwitch.frame.size.width - kFilterVCDefaultMargin, _enableFiltersSwitch.frame.origin.y, _enableFiltersSwitch.frame.size.width, _enableFiltersSwitch.frame.size.height)];
 
     if ([[UIDevice currentDevice].model isEqualToString:@"iPhone"]) {
-        UIApplication.sharedApplication.statusBarHidden=NO;
+        UIApplication.sharedApplication.statusBarHidden = NO;
 
     }
 }
