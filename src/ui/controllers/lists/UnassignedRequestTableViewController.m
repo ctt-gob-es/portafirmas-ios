e9db20a9d461cfcfa090b4fafa79927cf1748eac
SergioPH <spenin@solidgear.es>
2019-11-06 15:45:58 +0100

    Included super in a viewWillDisappear

--- src/ui/controllers/lists/UnassignedRequestTableViewController.m
+++ src/ui/controllers/lists/UnassignedRequestTableViewController.m
@@ -86,6 +86,7 @@
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+	[super viewWillDisappear: animated];
     [self.parentViewController setHidesBottomBarWhenPushed:TRUE];
     [self.navigationController setToolbarHidden:YES];
     [self.parentViewController.tabBarController.tabBar setHidden:NO];
