031ef3e7b839ccb2248a52bd9bb7c4d747c6e733
SergioPH <spenin@solidgear.es>
2019-11-13 09:03:33 +0100

    Fixed method declaration in login network .m

--- src/Network/LoginNetwork.m
+++ src/Network/LoginNetwork.m
@@ -55,7 +55,7 @@
     }] resume];
 }
 
-+ (void) loginWithRemoteCertificates:(void(^)(NSString *token))success failure:(void(^)(NSError *error))failure {
+- (void) loginWithRemoteCertificates:(void(^)(NSString *token))success failure:(void(^)(NSError *error))failure {
 	
 	NSString *opParameter = @"op";
 	NSString *datParameter = @"dat";
@@ -95,7 +95,7 @@
 	}] resume];
 }
 
-+ (void) validateLogin:(NSString*)certificate withSignedToken:(NSString*)tokenSigned success: (void(^)())success failure:(void(^)(NSError *error))failure {
+- (void) validateLogin:(NSString*)certificate withSignedToken:(NSString*)tokenSigned success: (void(^)())success failure:(void(^)(NSError *error))failure {
     NSString *opParameter = @"op";
     NSString *datParameter = @"dat";
     NSString *baseURL = SERVER_URL;
